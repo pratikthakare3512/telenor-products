c58d70162e31b41f6e0ed414af9decd8e6a8c0ee
Pratik <pratikthakare3512ggmail.com>
2020-08-24 23:29:54 +0530

    get all response json in prrtty print format

--- products/src/main/java/com/telenor/products/controller/controller.java
+++ products/src/main/java/com/telenor/products/controller/controller.java
@@ -2,20 +2,35 @@ package com.telenor.products.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.telenor.products.repository.ProductRepository;
 
+@RestController
 public class controller {
 	
 	@Autowired
 	private ProductRepository productRepo;
 	
-	@GetMapping("/product")
-	public Model showProducts(Model model) {
-				
-		model.addAttribute(productRepo);
-		return model;
+	@RequestMapping("/product")
+	public String showProducts(Model model) {
+		System.out.println("***** Inside The Controller *********");
+		model.addAttribute(productRepo.findAll());
+		System.out.println(model.toString());
+		String jsonString = "";
+		
+		ObjectMapper objectmapper = new ObjectMapper();
+		
+		try {
+			 jsonString = objectmapper.writerWithDefaultPrettyPrinter().writeValueAsString(model);
+			System.out.println("json :"+jsonString);
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+		}
+		return jsonString;
 		
 		
 	}
