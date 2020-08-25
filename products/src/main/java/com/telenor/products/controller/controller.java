4467a6f985b23a7c8d8e587ab6b05f01a9e390b2
Pratik <pratikthakare3512ggmail.com>
2020-08-25 12:48:30 +0530

    added search criteria for min and max price

--- products/src/main/java/com/telenor/products/controller/controller.java
+++ products/src/main/java/com/telenor/products/controller/controller.java
@@ -9,7 +9,6 @@ import javax.persistence.criteria.Root;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,29 +34,44 @@ public class controller {
 			@RequestParam(value = "property", required = false) String attribute,
 			@RequestParam(value = "value", required = false) String value,
 			@RequestParam(value = "city", required = false) String city,
-			@RequestParam(value = "min_price", required = false) String minPrice,
-			@RequestParam(value = "max_price", required = false) String maxPrice,
-			@RequestParam(value = "max_price", required = false) String gb_limit_min,
-			@RequestParam(value = "max_price", required = false) String gb_limit_max) throws JsonProcessingException {
+			@RequestParam(value = "min_price", required = false) Integer minPrice,
+			@RequestParam(value = "max_price", required = false) Integer maxPrice,
+			@RequestParam(value = "gb_limit_min", required = false) Integer gb_limit_min,
+			@RequestParam(value = "gb_limit_max", required = false) Integer gb_limit_max) throws JsonProcessingException {
 		String jsonString = "";
 		ObjectMapper objectmapper = new ObjectMapper();
 		System.out.println("***** Inside The Controller *********");
 		if (type != null || attribute != null || value != null || city != null || minPrice != null || maxPrice != null
 				|| gb_limit_min != null || gb_limit_max != null) {
-			System.out.println("***** Inside The Controller -Search Criteria *********");
 			jsonString = objectmapper.writerWithDefaultPrettyPrinter()
 					.writeValueAsString(productRepo.findAll(new Specification<Product>() {
 						@Override
 						public Predicate toPredicate(Root<Product> root, CriteriaQuery<?> cq, CriteriaBuilder cb) {
 							// TODO Auto-generated method stub
+												
 							Predicate p = cb.conjunction();
 							if (Objects.nonNull(type)) {
 								p = cb.and(p, cb.like(root.get("productType"), "%" + type + "%"));
 							}
+							if (Objects.nonNull(city)) {
+								p = cb.and(p, cb.like(root.get("city"), "%" + city + "%"));
+							}
+							if (Objects.nonNull(minPrice)) {
+								System.out.println("####price "+minPrice);
+								p = cb.and(p, cb.greaterThanOrEqualTo(root.get("price"), minPrice));
+							}
+							if (Objects.nonNull(maxPrice)) {
+								p = cb.and(p, cb.lessThanOrEqualTo(root.get("price"), maxPrice));
+							}
+							
 							return p;
 						}
 					}));
-			return "wow got the value " + jsonString;
+			if(!jsonString.equals("[ ]"))
+				return "Search Result : " + jsonString;
+			else {
+				 return "No matching values for current serach criteria !";
+			}
 		} else {
 			try {
 				jsonString = objectmapper.writerWithDefaultPrettyPrinter().writeValueAsString(productRepo.findAll());
